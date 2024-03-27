335717368ec7be294ce14c5f04a49060ff29cc12
Jarek Radosz <jarek@cvx.dev>
2024-03-27 17:39:18 +0100

    DEV: Update linting (#8)

--- javascripts/discourse/helpers/internal-or-external.js
+++ javascripts/discourse/helpers/internal-or-external.js
@@ -3,21 +3,15 @@ import { registerUnbound } from "discourse-common/lib/helpers";
 registerUnbound("shouldBeExternal", (navItem, selectedItem) => {
   let externalNavItems = settings.nav_items_to_exclude.split("|");
 
-  if (
-    // check to see if nav item is in excluded list, and that it does 
-    // not equal the currently selected item
-    externalNavItems.indexOf(navItem.name) !== -1 &&
+  // check to see if nav item is in excluded list, and that it does
+  // not equal the currently selected item
+  return (
+    externalNavItems.includes(navItem.name) &&
     navItem.name !== selectedItem.toLowerCase()
-  ) {
-    return true;
-  }
+  );
 });
 
-registerUnbound("shouldBeInternal", (navItem, selectedItem) => {
+registerUnbound("shouldBeInternal", (navItem) => {
   let externalNavItems = settings.nav_items_to_exclude.split("|");
-  if (externalNavItems.indexOf(navItem.name) !== -1) {
-    return false;
-  } else {
-    return true;
-  }
+  return !externalNavItems.includes(navItem.name);
 });
--- javascripts/discourse/initializers/custom-nav.js
+++ javascripts/discourse/initializers/custom-nav.js
@@ -1,13 +1,14 @@
-import { apiInitializer } from "discourse/lib/api";
 import { alias } from "@ember/object/computed";
+import { service } from "@ember/service";
+import { apiInitializer } from "discourse/lib/api";
 import discourseComputed from "discourse-common/utils/decorators";
-import { inject as service } from "@ember/service";
 
-export default apiInitializer("0.8", api => {
+export default apiInitializer("0.8", (api) => {
   api.modifyClass("component:navigation-bar", {
+    router: service(),
+
     classNameBindings: ["showCompactNav:compact-version"],
     showCompactVersion: alias("showCompactNav"),
-    router: service("router"),
     isMobile: alias("showMobileVersion"),
 
     @discourseComputed("site.isMobileDevice")
@@ -15,9 +16,13 @@ export default apiInitializer("0.8", api => {
       return isMobile;
     },
 
-    @discourseComputed("site.isMobileDevice", "router.currentRouteName", "router.currentURL")
+    @discourseComputed(
+      "site.isMobileDevice",
+      "router.currentRouteName",
+      "router.currentURL"
+    )
     showCompactNav(isMobile, routeName, currentURL) {
-      if(settings.everywhere){
+      if (settings.everywhere) {
         return true;
       }
 
@@ -26,18 +31,21 @@ export default apiInitializer("0.8", api => {
       let showOnCategory = settings.on_category_pages;
 
       // check for presence of 'tag' in routeName
-      let isTagPage = !!(routeName.match(/tag/));
+      let isTagPage = !!routeName.match(/tag/);
 
       // check for presence of 'categor' in routeName ie. categorIES, categorY will be matched
-      let isCategoryPage = !!(routeName.match(/categor/));
+      let isCategoryPage = !!routeName.match(/categor/);
 
       let isHomePage = currentURL === "/";
 
-      let renderOnTagePage = showOnTag === true && isTagPage === true;
+      let renderOnTagPage = showOnTag === true && isTagPage === true;
       let renderOnHomePage = showOnHome === true && isHomePage === true;
-      let renderOnCategoryPage = showOnCategory === true && isCategoryPage === true;
-            
-      return isMobile || renderOnTagePage || renderOnHomePage || renderOnCategoryPage;
-    }
+      let renderOnCategoryPage =
+        showOnCategory === true && isCategoryPage === true;
+
+      return (
+        isMobile || renderOnTagPage || renderOnHomePage || renderOnCategoryPage
+      );
+    },
   });
 });
