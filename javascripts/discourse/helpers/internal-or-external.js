0d7dabc2f2ebd9893550543b183d872e548fc709
David Taylor <david@taylorhq.com>
2024-03-27 16:33:21 +0000

    Update javascripts/discourse/helpers/internal-or-external.js

--- javascripts/discourse/helpers/internal-or-external.js
+++ javascripts/discourse/helpers/internal-or-external.js
@@ -3,8 +3,8 @@ import { registerUnbound } from "discourse-common/lib/helpers";
 registerUnbound("shouldBeExternal", (navItem, selectedItem) => {
   let externalNavItems = settings.nav_items_to_exclude.split("|");
 
-  // not equal the currently selected item
   // check to see if nav item is in excluded list, and that it does
+  // not equal the currently selected item
   return (
     externalNavItems.includes(navItem.name) &&
     navItem.name !== selectedItem.toLowerCase()
