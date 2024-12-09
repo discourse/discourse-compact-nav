cf06febf13ad6eabe07351c0e620464609107922
awesomerobot <kris.aubuchon@discourse.org>
2024-12-09 17:05:37 -0500

    remove old named file

--- migrations/settings/0001_migrate_icon_setting.js
+++
@@ -1,7 +0,0 @@
-export default function migrate(settings) {
-  if (settings.has("open_icon")) {
-    settings.set("dropdown_icon", settings.get("open_icon"));
-    settings.delete("open_icon");
-  }
-  return settings;
-}
