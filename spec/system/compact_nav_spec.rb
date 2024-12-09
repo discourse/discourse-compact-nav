420067a93f2790c887d4d723ede39fe51d3b3eb1
awesomerobot <kris.aubuchon@discourse.org>
2024-12-09 17:01:00 -0500

    fix migration, stree

--- spec/system/compact_nav_spec.rb
+++ spec/system/compact_nav_spec.rb
@@ -4,63 +4,63 @@ RSpec.describe "Compact Navigation Bar", system: true do
   let!(:theme) { upload_theme_component }
   fab!(:tag) { Fabricate(:tag, name: "bananas") }
 
-    it "shows the dropdown when the everywhere setting is set" do
-      theme.update_setting(:everywhere, true)
-      theme.save!
+  it "shows the dropdown when the everywhere setting is set" do
+    theme.update_setting(:everywhere, true)
+    theme.save!
 
-      visit "/"
+    visit "/"
 
-      expect(page).to have_css(".list-control-toggle-link-trigger") 
+    expect(page).to have_css(".list-control-toggle-link-trigger")
 
-      visit "/categories"
+    visit "/categories"
 
-      expect(page).to have_css(".list-control-toggle-link-trigger") 
+    expect(page).to have_css(".list-control-toggle-link-trigger")
 
-      visit "/tag/bananas"
+    visit "/tag/bananas"
 
-      expect(page).to have_css(".list-control-toggle-link-trigger") 
-    end
+    expect(page).to have_css(".list-control-toggle-link-trigger")
+  end
 
-    it "shows the dropdown on category routes and /categories" do
-      theme.update_setting(:on_category_pages, true)
-      theme.save!
+  it "shows the dropdown on category routes and /categories" do
+    theme.update_setting(:on_category_pages, true)
+    theme.save!
 
-      visit "/c/1"
+    visit "/c/1"
 
-      expect(page).to have_css(".list-control-toggle-link-trigger") 
+    expect(page).to have_css(".list-control-toggle-link-trigger")
 
-      visit "/categories"
+    visit "/categories"
 
-      expect(page).to have_css(".list-control-toggle-link-trigger")
+    expect(page).to have_css(".list-control-toggle-link-trigger")
 
-      visit "/"
+    visit "/"
 
-      expect(page).not_to have_css(".list-control-toggle-link-trigger") 
-    end
+    expect(page).not_to have_css(".list-control-toggle-link-trigger")
+  end
 
-    it "shows the dropdown on tag pages" do
-      theme.update_setting(:on_tag_pages, true)
-      theme.save!
+  it "shows the dropdown on tag pages" do
+    theme.update_setting(:on_tag_pages, true)
+    theme.save!
 
-      visit "/tag/bananas"
+    visit "/tag/bananas"
 
-      expect(page).to have_css(".list-control-toggle-link-trigger") 
+    expect(page).to have_css(".list-control-toggle-link-trigger")
 
-      visit "/categories"
+    visit "/categories"
 
-      expect(page).not_to have_css(".list-control-toggle-link-trigger")
-    end
- 
-    it "shows the dropdown on the home page" do
-      theme.update_setting(:on_home_page, true)
-      theme.save!
+    expect(page).not_to have_css(".list-control-toggle-link-trigger")
+  end
 
-      visit "/"
+  it "shows the dropdown on the home page" do
+    theme.update_setting(:on_home_page, true)
+    theme.save!
 
-      expect(page).to have_css(".list-control-toggle-link-trigger") 
+    visit "/"
 
-      visit "/c/1"
+    expect(page).to have_css(".list-control-toggle-link-trigger")
 
-      expect(page).not_to have_css(".list-control-toggle-link-trigger")
-    end
+    visit "/c/1"
+
+    expect(page).not_to have_css(".list-control-toggle-link-trigger")
+  end
 end
