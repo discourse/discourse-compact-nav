9c56f91bb816d1d1dcf75cb8385b5fe80208f63c
awesomerobot <kris.aubuchon@discourse.org>
2024-12-09 16:50:16 -0500

    DEV: refactor to remove overrides, update styles, settings

---
+++ javascripts/discourse/api-initializers/navigation-bar-override.js
@@ -0,0 +1,50 @@
+import { withPluginApi } from "discourse/lib/plugin-api";
+import { defaultHomepage } from "discourse/lib/utilities";
+
+const NAME = "navigation-bar-overrides";
+
+function showDropdownNavMode(isMobile, router) {
+  if (settings.everywhere) {
+    return true;
+  }
+
+  const routeName = router.currentRouteName;
+
+  const isTagPage = routeName === "tag.show";
+  const isCategoryPage = routeName === "discovery.category" || routeName === "discovery.categories";
+  const isHomePage = routeName === `discovery.${defaultHomepage()}`;
+
+  const showDropdownOnTagPage = settings.on_tag_pages && isTagPage;
+  const showDropdownOnHomePage = settings.on_home_page && isHomePage;
+  const showDropdownOnCategoryPage = settings.on_category_pages && isCategoryPage;
+
+  return (
+    isMobile || 
+    showDropdownOnTagPage || 
+    showDropdownOnHomePage || 
+    showDropdownOnCategoryPage
+  );
+};
+
+export default {
+  name: NAME,
+  initialize() {
+    withPluginApi("1.37.2", (api) => {
+      const mobileView =  api.container.lookup("service:site").mobileView;
+      const router =  api.container.lookup("service:router");
+      api.registerValueTransformer(
+        "navigation-bar-dropdown-mode",
+        () => {
+          console.log(showDropdownNavMode(mobileView, router))
+         return showDropdownNavMode(mobileView, router)
+        }
+      );
+      api.registerValueTransformer(
+        "navigation-bar-dropdown-icon",
+        () => {
+         return settings.dropdown_icon
+        }
+      );
+    });
+  },
+};
